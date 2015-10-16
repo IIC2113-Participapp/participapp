3903f94d514fb7ee8ce53161120433dd8fd87e1e
iaferrerPUC <iaferrer@uc.cl>
2015-10-16 10:07:04 -0300

    Crear Página de inicio, agregando CSS, JS e Imágenes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
+
+  root to: 'static_pages#home'
+
+  get 'static_pages/about'
+  get 'static_pages/contact'
+
   devise_for :users
   resources :forum
   resources :comment, only: [:create, :update, :destroy]
