5849c7c58ee831f70bda4637fbf2984923f60b58
Sebastian Salata R-T <SA.SalataRT@GMail.com>
2015-10-23 17:56:35 -0300

    fix migration error

--- db/migrate/20151015172011_add_editor_to_users.rb
+++ db/migrate/20151015172011_add_editor_to_users.rb
@@ -1,7 +1,5 @@
 class AddEditorToUsers < ActiveRecord::Migration
   def change
     add_column :users, :editor, :boolean, default: false
-    add_column :users, :default, :string
-    add_column :users, :false, :string
   end
 end
