3548fe4c8e7bba775c3fe3bea08a135b665c1168
Sebastian Salata R-T <SA.SalataRT@GMail.com>
2015-10-25 19:53:24 -0300

    scheduler now runs every 6 hours

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -2,6 +2,6 @@ require 'rubygems'
 require 'rake'
 require 'rufus/scheduler'
 scheduler = Rufus::Scheduler.new
-scheduler.every '15m' do
+scheduler.every '6h' do
   system 'bundle exec rake mailer:deliver_news'
 end
