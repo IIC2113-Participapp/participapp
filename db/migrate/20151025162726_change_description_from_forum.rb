b05163557c5a15a198d7c30c105b0b71e9721ebc
Sebastian Salata R-T <SA.SalataRT@GMail.com>
2015-10-29 12:20:02 -0300

    remove some hard tabs

--- db/migrate/20151025162726_change_description_from_forum.rb
+++ db/migrate/20151025162726_change_description_from_forum.rb
@@ -1,9 +1,9 @@
 class ChangeDescriptionFromForum < ActiveRecord::Migration
   def up
-  	change_column :forums, :description, :text
+    change_column :forums, :description, :text
   end
 
   def down
-  	change_column :forums, :description, :string
+    change_column :forums, :description, :string
   end
 end
