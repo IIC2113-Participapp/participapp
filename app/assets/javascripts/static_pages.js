56ddbe2936b0a440d30ea8555a501134916cfb45
Sebastian Salata R-T <SA.SalataRT@GMail.com>
2015-11-14 17:13:46 -0300

    remove hard tabs

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -1,11 +1,12 @@
 (function () {
 
 function homeEffects(){
-	$('.img-responsive').fadeTo("slow",1);
-	$('.name').fadeTo("slow",1);
-	$('.star-decoration').fadeTo("slow",1);
-	$('.sub-title').delay(500).fadeTo("slow",1);
+  $('.img-responsive').fadeTo("slow", 1);
+  $('.name').fadeTo("slow", 1);
+  $('.star-decoration').fadeTo("slow", 1);
+  $('.sub-title').delay(500).fadeTo("slow", 1);
 }
 
 $(document).on('page:change', homeEffects);
+
 })();
